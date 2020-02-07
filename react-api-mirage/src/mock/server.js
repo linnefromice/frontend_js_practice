8a77b0b59e63ae14471dd5e744e160dc060a540c
linnefromice <linnefromice@gmail.com>
2020-02-07 21:36:33 +0900

    add factories (only movie)

--- react-api-mirage/src/mock/server.js
+++ react-api-mirage/src/mock/server.js
@@ -7,12 +7,41 @@ export function makeServer({ environment = "development" } = {}) {
       user: Model,
       movie: Model,
     },
+    factories: {
+      movie: Factory.extend({
+        title(i) {
+          return `Movie Title ${i}`
+        },
+        releaseDate() {
+          const min = 19500101;
+          const max = 20201231;
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+        },
+        country() {
+          const countries = [
+            "America",
+            "United Kingdom",
+            "France",
+            "Italy",
+            "Japan",
+            "Korea",
+            "India",
+            "Russia"
+          ];
+          const randomIndex = Math.floor(Math.random() * (countries.length - 1));
+          return countries[randomIndex];
+        }
+      }),
+    },
     seeds(server) {
       server.create('user', { name: 'Bob' })
       server.create('user', { name: 'Alice' })
       server.create('movie', { title: 'The Intern', releaseDate: 20151010, country: 'America' })
       server.create('movie', { title: 'The Devil Wears Prada', releaseDate: 20061118, country: 'America' })
       server.create('movie', { title: 'Les Miserables', releaseDate: 20151221, country: 'United Kingdom' })
+      server.create('movie', { title: 'Fixed Movie Title 1' })
+      server.create('movie', { title: 'Fixed Movie Title 2', releaseDate: 20300101 })
+      server.create('movie')
     },
     routes() {
       this.namespace = 'api'
