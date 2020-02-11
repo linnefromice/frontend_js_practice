01e5afc8ac49ee641009eb9f68af301d4c4940fd
linnefromice <linnefromice@gmail.com>
2020-02-11 23:21:42 +0900

    modify marvel page's api of gatsby_api

--- gatsby-api/src/pages/page-marvel.js
+++ gatsby-api/src/pages/page-marvel.js
@@ -4,16 +4,18 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const url = "https://api.nytimes.com/svc/topstories/v2/home.json"
+const url = "https://gateway.marvel.com/v1/public/characters" // hard coding "characters"
+const ts = "1"
 const api_key = "XXXX"
+const hash = "f8f8379e2c88e5b003b7765cb1c730ea"
 
 const MarvelPage = () => {
   const [datas, setDatas] = useState([]);
   useEffect(() => {
-    fetch(`${url}?api-key=${api_key}`)
+    fetch(`${url}?ts=${ts}&apikey=${api_key}&hash=${hash}`)
       .then(response => response.json())
       .then(resultDatas => {
-        setDatas(resultDatas.results);
+        setDatas(resultDatas.data.results);
       })
     }, [])
 
@@ -23,7 +25,7 @@ const MarvelPage = () => {
       <Link to="/">Go back to the homepage</Link>
       <div>
         {datas.map((data) => 
-          <li>{data.title}</li>
+          <li>{data.name}</li>
         )}
       </div>
     </Layout>
