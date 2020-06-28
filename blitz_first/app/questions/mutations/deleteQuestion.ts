bc5bf219a3cec24cffd6478b01854476f05b8a2f
linnefromice <linnefromice@gmail.com>
2020-06-28 11:24:12 +0900

    update blitz project - modify delete logic (add choice)

--- blitz_first/app/questions/mutations/deleteQuestion.ts
+++ blitz_first/app/questions/mutations/deleteQuestion.ts
@@ -8,6 +8,7 @@ export default async function deleteQuestion(
   { where }: DeleteQuestionInput,
   ctx: Record<any, any> = {}
 ) {
+  await db.choice.deleteMany({where: {question: {id: where.id }}})
   const question = await db.question.delete({ where })
 
   return question
