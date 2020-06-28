ed0e9526b4538c763b394970ac6e59a0e4ac5cb1
linnefromice <linnefromice@gmail.com>
2020-06-29 01:43:40 +0900

    update blitz project - add header menu

--- blitz_first/app/pages/mypage.tsx
+++ blitz_first/app/pages/mypage.tsx
@@ -1,20 +1,49 @@
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles"
-import { AppBar, Toolbar, Typography } from "@material-ui/core"
+import React from "react"
+import { AppBar, Toolbar, Typography, Grid } from "@material-ui/core"
+import AdbIcon from "@material-ui/icons/Adb"
+import AccountCircleIcon from "@material-ui/icons/AccountCircle"
+import GitHubIcon from "@material-ui/icons/GitHub"
 
-const theme = createMuiTheme()
-const MyPage = () => (
-  <ThemeProvider theme={theme}>
-    <AppBar position="relative">
-      <Toolbar>
-        <Typography variant="h4">Blog - linnefromice</Typography>
-      </Toolbar>
-    </AppBar>
-    <div className="container">
-      <main>
-        <p>This is my page.</p>
-      </main>
-    </div>
-  </ThemeProvider>
+const Content = () => (
+  <div className="container">
+    <main>
+      <p>This is my page.</p>
+    </main>
+  </div>
 )
+
+const MyPage = () => {
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Grid
+            direction="row"
+            justify="space-between"
+            alignContent="center"
+            alignItems="center"
+            container
+          >
+            <Grid item align="center" xs={1}>
+              <AdbIcon fontSize="large" />
+            </Grid>
+            <Grid item xs={9}>
+              <Typography variant="h5" align="left">
+                Blog - linnefromice
+              </Typography>
+            </Grid>
+            <Grid item align="center" xs={1}>
+              <AccountCircleIcon fontSize="large" />
+            </Grid>
+            <Grid item align="center" xs={1}>
+              <GitHubIcon fontSize="large" />
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <Content />
+    </div>
+  )
+}
 
 export default MyPage
