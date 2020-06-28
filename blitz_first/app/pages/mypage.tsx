2dcafac036bfa37bb0a81ea9b79b369f308c2834
linnefromice <linnefromice@gmail.com>
2020-06-29 01:48:51 +0900

    update blitz project - refactor

--- blitz_first/app/pages/mypage.tsx
+++ blitz_first/app/pages/mypage.tsx
@@ -1,8 +1,5 @@
 import React from "react"
-import { AppBar, Toolbar, Typography, Grid } from "@material-ui/core"
-import AdbIcon from "@material-ui/icons/Adb"
-import AccountCircleIcon from "@material-ui/icons/AccountCircle"
-import GitHubIcon from "@material-ui/icons/GitHub"
+import Header from "app/layouts/header"
 
 const Content = () => (
   <div className="container">
@@ -15,32 +12,7 @@ const Content = () => (
 const MyPage = () => {
   return (
     <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Grid
-            direction="row"
-            justify="space-between"
-            alignContent="center"
-            alignItems="center"
-            container
-          >
-            <Grid item align="center" xs={1}>
-              <AdbIcon fontSize="large" />
-            </Grid>
-            <Grid item xs={9}>
-              <Typography variant="h5" align="left">
-                Blog - linnefromice
-              </Typography>
-            </Grid>
-            <Grid item align="center" xs={1}>
-              <AccountCircleIcon fontSize="large" />
-            </Grid>
-            <Grid item align="center" xs={1}>
-              <GitHubIcon fontSize="large" />
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
+      <Header />
       <Content />
     </div>
   )
