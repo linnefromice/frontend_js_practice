4dbf58c8603563468444713546e4e8f4969b1a9c
linnefromice <linnefromice@gmail.com>
2020-07-01 01:09:03 +0900

    update blitz project - add dummy data for bookmark page

--- blitz_first/app/pages/bookmark.tsx
+++ blitz_first/app/pages/bookmark.tsx
@@ -45,6 +45,63 @@ const articleLists = [
     url: "https://blog.logrocket.com/introduction-to-blitz-js/",
     rating: 2,
   },
+  {
+    imageUrl: "https://picsum.photos/200",
+    title: "Build an isomorphic application with Nuxt.js and Node",
+    date: 20200701,
+    tag: "Nuxt.js",
+    url: "https://blog.logrocket.com/build-an-isomorphic-application-with-nuxt-js-and-node/",
+    rating: 2,
+  },
+  {
+    imageUrl: "https://picsum.photos/200",
+    title: "Creating Dynamic Routes in a Nuxt Application",
+    date: 20200701,
+    tag: "Nuxt.js",
+    url: "https://css-tricks.com/creating-dynamic-routes-in-a-nuxt-application/",
+    rating: 2,
+  },
+  {
+    imageUrl: "https://picsum.photos/200",
+    title: "How to set up and code Nuxt.js apps fully in TypeScript",
+    date: 20200701,
+    tag: "Nuxt.js",
+    url: "https://blog.logrocket.com/how-to-set-up-and-code-nuxt-js-apps-fully-in-typescript/",
+    rating: 3,
+  },
+  {
+    imageUrl: "https://picsum.photos/200",
+    title: "Getting Started with Nuxt.js",
+    date: 20200701,
+    tag: "Nuxt.js",
+    url: "https://www.telerik.com/blogs/getting-started-with-nuxtjs",
+    rating: 3,
+  },
+  {
+    imageUrl: "https://picsum.photos/200",
+    title: "7 Frontend Architecture Lessons From Nuxt.js",
+    date: 20200701,
+    tag: "Nuxt.js",
+    url: "https://zendev.com/2018/09/17/frontend-architecture-lessons-from-nuxt-js.html",
+    rating: 3,
+  },
+  {
+    imageUrl: "https://picsum.photos/200",
+    title: "How to Use Flat-File Data in a Static Nuxt App",
+    date: 20200701,
+    tag: "Nuxt.js",
+    url: "https://www.freecodecamp.org/news/how-to-use-flat-file-data-in-a-static-nuxt-app/",
+    rating: 1,
+  },
+  {
+    imageUrl: "https://picsum.photos/200",
+    title: "Deep dive into Gatsby — Building a static blog using Gatsby, React and GraphQL",
+    date: 20200701,
+    tag: "Gatsby.js",
+    url:
+      "https://levelup.gitconnected.com/deep-dive-into-gatsby-building-a-static-blog-using-gatsby-react-and-graphql-f8cb8d8fd036",
+    rating: 2,
+  },
 ]
 
 const BookmarkCard = ({ data }) => (
