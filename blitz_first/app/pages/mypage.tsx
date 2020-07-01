404eacfe8d18eb4317e7ff580f896464f121e534
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-07-01 20:22:17 +0900

    update blitz project - update mypage (use circular progress / temp ui)

--- blitz_first/app/pages/mypage.tsx
+++ blitz_first/app/pages/mypage.tsx
@@ -1,13 +1,46 @@
 import BaseContainer from "app/layouts/base_container"
+import { makeStyles } from "@material-ui/core/styles"
+import CircularProgress from "@material-ui/core/CircularProgress"
+import CodeIcon from "@material-ui/icons/Code"
+import GitHubIcon from "@material-ui/icons/GitHub"
 
-const MyPage = () => (
-  <BaseContainer>
-    <div className="container">
-      <main>
-        <p>This is my page.</p>
-      </main>
-    </div>
-  </BaseContainer>
-)
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+  },
+  wrapper: {
+    margin: theme.spacing(1),
+    position: "relative",
+  },
+  progress: {
+    position: "absolute",
+    zIndex: -1,
+  },
+  skill: {
+    position: "absolute",
+  },
+}))
+
+const MyPage = () => {
+  const classes = useStyles()
+
+  return (
+    <BaseContainer>
+      <div className={classes.root}>
+        <div className={classes.wrapper}>
+          <div>
+            <CircularProgress className={classes.progress} size={200} variant="static" value={75} />
+            <CodeIcon className={classes.skill} />
+          </div>
+          <div>
+            <CircularProgress className={classes.progress} size={200} variant="static" value={40} />
+            <GitHubIcon className={classes.skill} />
+          </div>
+        </div>
+      </div>
+    </BaseContainer>
+  )
+}
 
 export default MyPage
