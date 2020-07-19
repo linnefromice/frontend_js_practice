5290a1489ebc095d00388c7552e7d3e57e323008
linnefromice <linnefromice@gmail.com>
2020-07-20 01:31:57 +0900

    update react-fluentui project - create CardsSection

--- react-fluentui/src/App.js
+++ react-fluentui/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "office-ui-fabric-react/dist/css/fabric.css";
 import "./App.css";
 import Navigation from "./components/Navigation";
+import CardsSection from "./components/CardSection";
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
           <Navigation />
         </div>
         <div className="main-element ms-Grid-col ms-sm10 ms-xl10">
-          <div className="ms-Grid-row"></div>
+          <div className="ms-Grid-row">
+            <CardsSection />
+          </div>
           <div className="ms-Grid-row"></div>
         </div>
       </div>
