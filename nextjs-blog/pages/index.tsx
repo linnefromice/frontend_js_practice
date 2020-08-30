87f5770ab8ca9dc710310c9b3c3965b80d1dfad5
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-08-31 01:47:12 +0900

    application to typescript

--- nextjs-blog/pages/index.tsx
+++ nextjs-blog/pages/index.tsx
@@ -4,8 +4,17 @@ import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
+import { GetStaticProps } from "next";
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}) {
   return (
     <Layout home>
       <Head>
@@ -40,11 +49,11 @@ export default function Home({ allPostsData }) {
   );
 }
 
-export async function getStaticProps() {
+export const getStaticProps = async () => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
       allPostsData,
     },
   };
-}
+};
