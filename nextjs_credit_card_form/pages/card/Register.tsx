199426a4b452dac2de96de0347d89535eec21823
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-09-03 01:59:24 +0900

    update credit card - modify card/form styles

--- nextjs_credit_card_form/pages/card/Register.tsx
+++ nextjs_credit_card_form/pages/card/Register.tsx
@@ -3,7 +3,8 @@ import styles from "./Register.module.scss";
 const Register = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.wrapper__contentArea}>CREDIT CARD FORM</div>
+      <div className={styles.wrapper__cardArea}>Credit Card</div>
+      <div className={styles.wrapper__formArea}>Register Form</div>
     </div>
   );
 };
