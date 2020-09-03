1b6c53270f68e44a7cc8981108ed3725df792d8c
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-09-04 02:10:48 +0900

    update credit card - add form components (no style)

--- nextjs_credit_card_form/pages/card/Register.tsx
+++ nextjs_credit_card_form/pages/card/Register.tsx
@@ -4,7 +4,35 @@ const Register = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.wrapper__cardArea}>Credit Card</div>
-      <div className={styles.wrapper__formArea}>Register Form</div>
+      <div className={styles.wrapper__formArea}>
+        <div className={styles.card_form}>
+          <div className={styles.card_form__inner}>
+            <div className={styles.card_form__row}>
+              <div className={styles.card_form__group}>
+                <label>Card Number</label>
+                <input type="text" />
+              </div>
+            </div>
+            <div className={styles.card_form__row}>
+              <div className={styles.card_form__group}>
+                <label>Card Name</label>
+                <input type="text" />
+              </div>
+            </div>
+            <div className={styles.card_form__row}>
+              <div className={styles.card_form__group}>
+                <label>Expiration Date</label>
+                <input type="text" />
+                <input type="text" />
+              </div>
+              <div className={styles.card_form__group}>
+                <label>CVV</label>
+                <input type="number" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
