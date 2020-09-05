ff105fa2eecc0e3e9de21a15a9af32d5b08d9efc
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-09-06 01:46:21 +0900

    update credit card - add ic area

--- nextjs_credit_card_form/pages/card/View.tsx
+++ nextjs_credit_card_form/pages/card/View.tsx
@@ -4,7 +4,15 @@ const View = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.wrapper__row}>
-        <div className={styles.card__ic}>IC</div>
+        <div className={styles.card__ic}>
+          <div className={styles.ic_area}>
+            <div className={styles.ic_area__content}>
+              <div className={styles.ic_area__inline_one}>
+                <div className={styles.ic_area__inline_two}></div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className={styles.card__brand_name}>VISA</div>
       </div>
       <div className={styles.wrapper__row}>
