9aad2c35407d3060da77f6da679a4f11da4d7bc9
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-09-07 14:38:19 +0900

    update credit card - update form (add logic / refactor)

--- nextjs_credit_card_form/pages/card/Register.tsx
+++ nextjs_credit_card_form/pages/card/Register.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./Register.module.scss";
 import View from "./View";
 
@@ -6,8 +6,34 @@ const years = [...Array(20).keys()].map((i) => (i += 2020).toString());
 const months = [...Array(12).keys()].map((i) => (i += 1).toString());
 
 const Register = () => {
-  const [year, setYear] = useState("YEAR");
-  const [month, setMonth] = useState("MONTH");
+  const [cardNumber, setCardNumber] = useState<string>("");
+  const [holderName, setHolderName] = useState<string>("");
+  const [year, setYear] = useState<string>("YEAR");
+  const [month, setMonth] = useState<string>("MONTH");
+  const [cvv, setCvv] = useState<string>("");
+
+  // for debug
+  useEffect(() => {
+    console.log(`cardNumber: ${cardNumber}`);
+  }, [cardNumber]);
+
+  const processDisplayedCardNumber = (value: string): string => {
+    return Array.from(value)
+      .map((element, index) => {
+        return index % 4 == 3 ? element + " " : element;
+      })
+      .join("")
+      .substring(0, 19);
+  };
+  const saveCardNumber = (value: string) => {
+    let number =
+      value.length <= 19
+        ? value.replace(/\s+/g, "")
+        : value.substring(0, 19).replace(/\s+/g, "");
+    number == cardNumber && value.length != 20
+      ? setCardNumber(number.slice(0, -1))
+      : setCardNumber(number);
+  };
 
   return (
     <div className={styles.wrapper}>
@@ -22,7 +48,12 @@ const Register = () => {
                 className={`${styles.card_form__group} ${styles.card_input}`}
               >
                 <label className={styles.card_input__label}>Card Number</label>
-                <input className={styles.card_input__input} type="text" />
+                <input
+                  className={styles.card_input__input}
+                  type="text"
+                  value={processDisplayedCardNumber(cardNumber)}
+                  onChange={(e) => saveCardNumber(e.target.value)}
+                />
               </div>
             </div>
             <div className={styles.card_form__row}>
@@ -30,7 +61,12 @@ const Register = () => {
                 className={`${styles.card_form__group} ${styles.card_input}`}
               >
                 <label className={styles.card_input__label}>Card Name</label>
-                <input className={styles.card_input__input} type="text" />
+                <input
+                  className={styles.card_input__input}
+                  type="text"
+                  value={holderName}
+                  onChange={(e) => setHolderName(e.target.value)}
+                />
               </div>
             </div>
             <div className={styles.card_form__row}>
@@ -64,7 +100,12 @@ const Register = () => {
               <div className={styles.card_form__group_half}>
                 <div className={styles.card_input}>
                   <label className={styles.card_input__label}>CVV</label>
-                  <input className={styles.card_input__input} type="text" />
+                  <input
+                    className={styles.card_input__input}
+                    type="text"
+                    value={cvv}
+                    onChange={(e) => setCvv(e.target.value)}
+                  />
                 </div>
               </div>
             </div>
