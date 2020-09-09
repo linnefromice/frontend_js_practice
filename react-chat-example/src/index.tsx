759d2bbac5525604b83c1a45f1846b89fdc39cdf
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-09-10 00:16:07 +0900

    update chat pj - add clientside

--- react-chat-example/src/index.tsx
+++ react-chat-example/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { startWebsocketConnection } from "./websocket";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +11,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+startWebsocketConnection()
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
