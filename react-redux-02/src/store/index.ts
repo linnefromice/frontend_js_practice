bdb23a44d5f675b3d49e4d4068f5972288c1d412
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-09-16 02:06:30 +0900

    add redux global setting & add files for task's store

---
+++ react-redux-02/src/store/index.ts
@@ -0,0 +1,9 @@
+import { create } from "core-js/fn/object";
+import { combineReducers, createStore } from "redux"
+
+const rootReducer = combineReducers({
+    // TODO
+});
+export type RootState = ReturnType<typeof rootReducer>
+const store = createStore(rootReducer)
+export default store
