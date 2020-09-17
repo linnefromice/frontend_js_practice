90c5a4e21ff4327abf5a5d5266f183b242a1275f
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-09-18 00:28:32 +0900

    add form for updating exist task

--- react-redux-02/src/components/task/Task.tsx
+++ react-redux-02/src/components/task/Task.tsx
@@ -1,7 +1,18 @@
-import React from "react"
+import React, { useState, useCallback } from "react"
+import { useDispatch } from "react-redux"
 import "./Task.scss"
 
 export const Task = ({id, status, title}: {id: number, status: string, title: string}) => {
+    const dispatch = useDispatch()
+    const [inputTitle, setInputTitle] = useState(title)
+    const [selectStatus, setSelectStatus] = useState(status)
+    const handleInputTitle = useCallback((event: React.ChangeEvent<HTMLInputElement>): void => {
+        setInputTitle(event.target.value)
+    }, [])
+    const handleSelectStatus = useCallback((event: React.ChangeEvent<HTMLSelectElement>): void => {
+        setSelectStatus(event.target.value)
+    }, [])
+
     return (
         <div className="Task">
             <div className="view_task_update_area">
@@ -10,12 +21,20 @@ export const Task = ({id, status, title}: {id: number, status: string, title: st
             <div className="view_task_delete_area">
                 DELETE
             </div>
-            <div className="view_task_status_area">
-                {status}
-            </div>
-            <div className="view_task_input_area">
-                {title}
-            </div>
+            <select
+                className="view_task_status_area"
+                onChange={handleSelectStatus}
+            >
+                {status === "PENDING" ? <option value="PENDING" selected>着手不可</option> : <option value="PENDING">着手不可</option>}
+                {status === "READY" ? <option value="READY" selected>未着手</option> : <option value="READY">未着手</option>}
+                {status === "DOING" ? <option value="DOING" selected>着手中</option> : <option value="DOING">着手中</option>}
+                {status === "DONE" ? <option value="DONE" selected>完了</option> : <option value="DONE">完了</option>}
+            </select>
+            <input
+                className="view_task_input_area"
+                onChange={handleInputTitle}
+                value={inputTitle}>
+            </input>
         </div>
     )
 }
