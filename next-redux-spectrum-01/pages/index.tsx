149f92ef62fc4d85d01546bdb27538f6ecd03f52
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-09-24 23:32:47 +0900

    support SSR

--- next-redux-spectrum-01/pages/index.tsx
+++ next-redux-spectrum-01/pages/index.tsx
@@ -1,15 +1,17 @@
 import { CreateTask } from '../components/CreateTask'
 import Layout from '../components/Layout'
-import {Provider, defaultTheme } from '@adobe/react-spectrum';
+import { SSRProvider, Provider, defaultTheme } from '@adobe/react-spectrum';
 import { TaskList } from '../components/TaskList'
 
 const IndexPage = () => (
-  <Provider colorScheme="dark" theme={defaultTheme}>
-    <Layout title="Home | Next.js + TypeScript Example">
-      <CreateTask/>
-      <TaskList/>
-    </Layout>
-  </Provider>
+  <SSRProvider>
+    <Provider colorScheme="dark" theme={defaultTheme}>
+      <Layout title="Home | Next.js + TypeScript Example">
+        <CreateTask/>
+        <TaskList/>
+      </Layout>
+    </Provider>
+  </SSRProvider>
 )
 
 export default IndexPage
