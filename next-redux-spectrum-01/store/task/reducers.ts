6f92113fa67b3d0792dcd98d16e4eabdf71b7ece
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-09-25 01:08:36 +0900

    add reducers

---
+++ next-redux-spectrum-01/store/task/reducers.ts
@@ -0,0 +1,47 @@
+import { ActionTypes, TaskActionTypes, Tasks } from "./types"
+const initialState: Tasks = [
+    {
+        id: 1,
+        status: "DONE",
+        title: "Create Logic in Service class"
+    },
+    {
+        id: 2,
+        status: "DOING",
+        title: "Create View in Login Component"
+    },
+    {
+        id: 3,
+        status: "READY",
+        title: "Use Redux to frontend entire"
+    },
+    {
+        id: 4,
+        status: "PENDING",
+        title: "Data Migration"
+    },
+]
+export const reducers = (state = initialState, action: TaskActionTypes) => {
+    switch (action.type) {
+        case ActionTypes.createTask:
+            const latestId = state.length
+            return [...state, {
+                id: latestId + 1,
+                status: action.payload.status,
+                title: action.payload.title,
+            }]
+        case ActionTypes.updateTask:
+            return state.filter((data) => {
+                if (data.id === action.payload.id) {
+                    data.status = action.payload.status
+                    data.title = action.payload.title
+                }
+                return data
+            })
+        case ActionTypes.deleteTask:
+            return state.filter((data) => data.id !== action.payload.id)
+        default:
+            const _: never = action;
+            return state
+    }
+}
