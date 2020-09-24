572f0b398c03eaf0a09b63c413c9acf9fd119fdd
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-09-24 23:36:26 +0900

    centering

--- next-redux-spectrum-01/pages/index.tsx
+++ next-redux-spectrum-01/pages/index.tsx
@@ -1,14 +1,20 @@
 import { CreateTask } from '../components/CreateTask'
 import Layout from '../components/Layout'
-import { SSRProvider, Provider, defaultTheme } from '@adobe/react-spectrum';
+import { SSRProvider, Provider, defaultTheme, Flex } from '@adobe/react-spectrum';
 import { TaskList } from '../components/TaskList'
 
 const IndexPage = () => (
   <SSRProvider>
     <Provider colorScheme="dark" theme={defaultTheme}>
       <Layout title="Home | Next.js + TypeScript Example">
-        <CreateTask/>
-        <TaskList/>
+        <Flex
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+          >
+          <CreateTask/>
+          <TaskList/>
+        </Flex>
       </Layout>
     </Provider>
   </SSRProvider>
