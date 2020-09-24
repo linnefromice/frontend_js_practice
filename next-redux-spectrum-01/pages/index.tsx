6308d15ff611d3c67fb8909d581b5c68a8ca070e
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-09-25 01:21:04 +0900

    apply reducers & store

--- next-redux-spectrum-01/pages/index.tsx
+++ next-redux-spectrum-01/pages/index.tsx
@@ -1,14 +1,18 @@
 import Layout from '../components/Layout'
-import { SSRProvider, Provider, defaultTheme } from '@adobe/react-spectrum';
+import { SSRProvider, Provider as SpectrumProvider, defaultTheme } from '@adobe/react-spectrum';
+import { Provider as ReduxProvider } from "react-redux"
+import store from "../store"
 import MainContent from '../components/MainContent';
 
 const IndexPage = () => (
   <SSRProvider>
-    <Provider colorScheme="dark" theme={defaultTheme}>
-      <Layout title="Home | Next.js / React Spectrum / Redux">
-        <MainContent/>
-      </Layout>
-    </Provider>
+    <ReduxProvider store={store}>
+      <SpectrumProvider colorScheme="dark" theme={defaultTheme}>
+        <Layout title="Home | Next.js / React Spectrum / Redux">
+          <MainContent/>
+        </Layout>
+      </SpectrumProvider>
+    </ReduxProvider>
   </SSRProvider>
 )
 
--- next-redux-spectrum-01/store/index.ts
+++ next-redux-spectrum-01/store/index.ts
@@ -1,6 +1,9 @@
 import { combineReducers, createStore } from "redux"
+import { reducers as taskReducers } from "./task/reducers"
 
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({
+    tasks: taskReducers
+})
 export type RootState = ReturnType<typeof rootReducer>
 const store = createStore(rootReducer)
 export default store
