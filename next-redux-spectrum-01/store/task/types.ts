63a54f637680f5645c3edd20ba9a2a558b320052
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-09-25 00:50:53 +0900

    add types

---
+++ next-redux-spectrum-01/store/task/types.ts
@@ -0,0 +1,37 @@
+import { Action } from "redux"
+
+export const ActionTypes = {
+    createTask: "CREATE_TASK",
+    updatgeTask: "UPDATE_TASK",
+    deleteTask: "DELETE_TASK",
+} as const
+
+type TaskData = {
+    id: number
+    status: string
+    title: string
+}
+export type Tasks = TaskData[]
+
+interface CreateTaskAction extends Action {
+    type: typeof ActionTypes.createTask
+    payload: {
+        status: string
+        title: string
+    }
+}
+interface UpdateTaskAction extends Action {
+    type: typeof ActionTypes.updatgeTask
+    payload: {
+        id: number
+        status: string
+        title: string
+    }
+}
+interface DeleteTaskAction extends Action {
+    type: typeof ActionTypes.deleteTask
+    payload: {
+        id: number
+    }
+}
+export type TaskActionTypes = CreateTaskAction | UpdateTaskAction | DeleteTaskAction
