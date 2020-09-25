031927f3c83ed8ce2531f77f34f78ab605092bd0
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-09-25 11:37:21 +0900

    use store when creating task

--- next-redux-spectrum-01/components/CreateTask.tsx
+++ next-redux-spectrum-01/components/CreateTask.tsx
@@ -7,8 +7,25 @@ import {
     Text, View
 } from '@adobe/react-spectrum'
 import AddCircle from '@spectrum-icons/workflow/AddCircle'
+import { useState, useCallback } from "react"
+import { useDispatch } from "react-redux"
+import { createTaskAction } from "../store/task/actions"
 
 export const CreateTask = () => {
+    const dispatch = useDispatch()
+    const [selectState, setSelectStatus] = useState("PENDING")
+    const [inputTitle, setInputTitle] = useState("")
+    const handleSelectStatus = useCallback((value: string): void => {
+        setSelectStatus(value)
+    }, [])
+    const handleInputTitle = useCallback((value: string): void => {
+        setInputTitle(value)
+    }, [])
+    const handleCreateTask = () => {
+        dispatch(createTaskAction(selectState, inputTitle))
+        setInputTitle("")
+    }
+
     return (
         <Flex
             direction="row"
@@ -18,10 +35,17 @@ export const CreateTask = () => {
             gap="size-100"
         >
             <View>
-                <TextField label="title" placeholder="Input your new task!"/>
+                <TextField
+                    label="title"
+                    placeholder="Input your new task!"
+                    onChange={handleInputTitle}
+                />
             </View>
             <View width="size-2000">
-                <RadioGroup label="Status">
+                <RadioGroup
+                    label="Status"
+                    onChange={handleSelectStatus}
+                >
                     <Radio value="PENDING">着手不可</Radio>
                     <Radio value="READY">未着手</Radio>
                     <Radio value="DOING">着手中</Radio>
@@ -29,7 +53,7 @@ export const CreateTask = () => {
                 </RadioGroup>
             </View>
             <View>
-                <ActionButton>
+                <ActionButton onPress={handleCreateTask}>
                     <AddCircle />
                     <Text>Add</Text>
                 </ActionButton>
