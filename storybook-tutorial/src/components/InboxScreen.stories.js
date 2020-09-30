8a1d9adfb4d97c39ce2d3e175c7c32104577d9a2
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-10-01 01:04:39 +0900

    create InboxScreen storybook

---
+++ storybook-tutorial/src/components/InboxScreen.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { PureInboxScreen } from './InboxScreen'
+
+export default {
+    conponent: PureInboxScreen,
+    title: 'InboxScreen',
+}
+
+const Template = args => <PureInboxScreen {...args} />
+export const Default = Template.bind({})
+export const Error = Template.bind({})
+Error.args = {
+    error: 'Something'
+}
