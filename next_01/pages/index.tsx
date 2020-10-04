898442f405735744c99a0e9be8bfb829bb9b3eeb
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-10-05 00:44:29 +0900

    create css using tailwind & apply to view

--- next_01/pages/index.tsx
+++ next_01/pages/index.tsx
@@ -2,7 +2,7 @@ import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+    <h1 className="text-6xl font-semibold italic">Hello Next.js 👋</h1>
     <p>
       IndexPage
     </p>
