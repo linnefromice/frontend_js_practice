e914c9082b6a71290ca2494901b69c355e291211
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-10-06 02:00:37 +0900

    update contents pages

--- next_01/pages/index.tsx
+++ next_01/pages/index.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1 className="text-6xl font-semibold italic">Hello Next.js 👋</h1>
-    <p>
-      IndexPage
+    <h1 className="text-6xl font-bold">Hello Next.js 👋</h1>
+    <p className="text-l font-semibold italic">
+      <Link href="/repository">
+        <a>Go Repository</a>
+      </Link>
     </p>
   </Layout>
 )
--- next_01/pages/repository.tsx
+++ next_01/pages/repository.tsx
@@ -3,13 +3,18 @@ import Layout from '../components/Layout'
 
 const RepositoryPage = () => (
   <Layout title="Repository | Next.js + TypeScript Example">
-    <h1>Repository</h1>
-    <p>This is the Repository page</p>
-    <p>
-      <Link href="/">
-        <a>Go Repository</a>
+    <h1 className="text-6xl font-bold">Repsoitory Page</h1>
+    <p className="m-2 text-l font-semibold italic">
+      <Link href="/repository">
+        <a>Go Home</a>
       </Link>
     </p>
+    <div className="m-2 flex flex-col">
+      <p className="font-serif text-lg text-gray-800 italic">front_js_practice</p>
+      <p className="font-serif text-lg text-gray-800 italic">ruby_practice_basic</p>
+      <p className="font-serif text-lg text-gray-800 italic">practice_design</p>
+      <p className="font-serif text-lg text-gray-800 italic">portfolio_one</p>
+    </div>
   </Layout>
 )
 
