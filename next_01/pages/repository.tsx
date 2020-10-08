833c384389ecaee3ca606d11af76f834139beff4
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-10-08 11:58:25 +0900

    update design - update search box & add search result display

--- next_01/pages/repository.tsx
+++ next_01/pages/repository.tsx
@@ -2,23 +2,38 @@ import Layout from '../components/Layout'
 
 const RepositoryPage = () => (
   <Layout title="Repository | Next.js + TypeScript Example">
-    <div className="my-2 flex flex-row">
-      <div className="w-7/12 p-1">
-        <input className="w-full h-full border border-gray-500 rounded border-opacity-50 p-1" type="text" placeholder="Find a repository..."></input>
-      </div>
-      <div className="w-2/12 p-1">
-        <div className="border border-gray-500 rounded border-opacity-50 p-1">
-          <span className="text-gray-700 text-sm">Type: All ▼</span>
+    <div>
+      <div className="my-2 flex flex-row">
+        <div className="w-7/12 p-1">
+          <input className="w-full h-full border border-gray-500 rounded border-opacity-50 p-1" type="text" placeholder="Find a repository..."></input>
+        </div>
+        <div className="w-2/12 p-1">
+          <div className="border border-gray-500 rounded border-opacity-50 p-1">
+            <span className="text-gray-700 text-sm">Type: All ▼</span>
+          </div>
+        </div>
+        <div className="w-2/12 p-1">
+          <div className="border border-gray-500 rounded border-opacity-50 p-1">
+            <span className="text-gray-700 text-sm">Language: All ▼</span>
+          </div>
+        </div>
+        <div className="w-1/12 p-1">
+          <div className="rounded bg-green-500 hover:bg-green-700 p-1">
+            <span className="text-white">New</span>
+          </div>
         </div>
       </div>
-      <div className="w-2/12 p-1">
-        <div className="border border-gray-500 rounded border-opacity-50 p-1">
-          <span className="text-gray-700 text-sm">Language: All ▼</span>
+      <div className="my-2 flex flex-row">
+        <div className="w-5/6 p-1">
+          <div>
+            <span className="font-semibold">30</span>
+            <span className="text-gray-700 text-sm"> results for repositories matching </span>
+            <span>practice</span>
+          </div>
         </div>
-      </div>
-      <div className="w-1/12 p-1">
-        <div className="rounded bg-green-500 hover:bg-green-700 p-1">
-          <span className="text-white">New</span>
+        <div className="w-1/6 p-1 flex flex-row justify-center items-center">
+          <div className="mx-1 px-2 bg-gray-700 border rounded-lg border-solid text-white text-lg">×</div>
+          <div className="mx-1 text-gray-700">Clear filter</div>
         </div>
       </div>
     </div>
