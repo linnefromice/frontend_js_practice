b4bd50d3983d932dff56a4b6ed5945fb65e58287
linnefromice <linnefromice@gmail.com>
2020-10-16 17:05:46 +0900

    add stats component

--- react-recoil-02/src/components/TodoList.js
+++ react-recoil-02/src/components/TodoList.js
@@ -119,10 +119,46 @@ const TodoItem = ({ item }) => {
   );
 };
 
+const todoListStatsState = selector({
+  key: "todoListStatsState",
+  get: ({ get }) => {
+    const todoList = get(todoListState);
+    const totalNum = todoList.length;
+    const totalCompletedNum = todoList.filter((item) => item.isComplete).length;
+    const totalUncompletedNum = totalNum - totalCompletedNum;
+    const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum;
+    return {
+      totalNum,
+      totalCompletedNum,
+      totalUncompletedNum,
+      percentCompleted,
+    };
+  },
+});
+const TodoListStats = () => {
+  const {
+    totalNum,
+    totalCompletedNum,
+    totalUncompletedNum,
+    percentCompleted,
+  } = useRecoilValue(todoListStatsState);
+  const formattedPercentCompleted = Math.round(percentCompleted * 100);
+
+  return (
+    <ul>
+      <li>Total items: {totalNum}</li>
+      <li>Items completed: {totalCompletedNum}</li>
+      <li>Items not completed: {totalUncompletedNum}</li>
+      <li>Percent completed: {formattedPercentCompleted}</li>
+    </ul>
+  );
+};
+
 const TodoList = () => {
   const todoList = useRecoilValue(filteredTodoListState);
   return (
     <>
+      <TodoListStats />
       <TodoListFilters />
       <TodoItemCreator />
       {todoList.map((todoItem) => (
