70f852cbc6565e298518946d80a6bba57b227da7
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-10-18 01:20:13 +0900

    prepare using apollo client

--- next_01/pages/_app.js
+++ next_01/pages/_app.js
@@ -1,7 +1,29 @@
 import "../src/styles/index.css";
+import { ApolloProvider, createHttpLink, ApolloClient, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
+const AUTH_TOKEN = "XXXX"
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+})
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `token ${AUTH_TOKEN}`,
+    }
+  }
+})
+const client = new ApolloClient({
+  uri: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
+}
 
 export default App;
