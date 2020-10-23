527f50312bbc58e2f8021a60479a5c321e3c373f
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-10-23 10:13:06 +0900

    create component expanding children props

--- react-design-pattern-01/src/separate_presen_view/Summary.tsx
+++ react-design-pattern-01/src/separate_presen_view/Summary.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import TextButton from "../moleculars/TextButton"
+import LoggerChildrenProps from "./LoggerChildrenProps"
 import withLogger from "./LoggerHOC"
 
 const LogTextButton = ({ log }: { log?: string }) => (
@@ -17,8 +18,12 @@ const Summary = () => {
     return (
         <div>
             <h2>HOC</h2>
-            <WrapTextButton log="high order components" />
+            <WrapTextButton log="[LOG] high order components" />
             <h2>childrenのprops拡張</h2>
+            <LoggerChildrenProps log="[LOG] with children props">
+                <LogTextButton />
+                ##文字列にはinjectionしない##
+            </LoggerChildrenProps>
             <h2>componentのprops渡し</h2>
         </div>
     )
