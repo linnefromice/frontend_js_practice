561191df1457057072493f669d9e843a4f4ed2c8
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-11-03 21:00:20 +0900

    update pages

--- next_03/pages/index.tsx
+++ next_03/pages/index.tsx
@@ -1,14 +1,26 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
-import { _BaseButton } from "../components/atoms/_BaseButton"
+import { PrimaryButton } from '../components/atoms/PrimaryButton'
+import { SecondaryButton } from '../components/atoms/SecondaryButton'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
     <p>
-      <_BaseButton>
-        <Link href="/about">About</Link>
-      </_BaseButton>
+      <Link href="/">Home</Link>
+    </p>
+    <p>
+      <Link href="/about">About</Link>
+    </p>
+    <p>
+      <PrimaryButton>
+        Primary
+      </PrimaryButton>
+    </p>
+    <p>
+      <SecondaryButton>
+        Secondary
+      </SecondaryButton>
     </p>
   </Layout>
 )
