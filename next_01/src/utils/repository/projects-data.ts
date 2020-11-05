98831f61b381024caf519204bf12dc1ead175fb2
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-11-06 02:12:59 +0900

    apply interface

--- next_01/src/utils/repository/projects-data.ts
+++ next_01/src/utils/repository/projects-data.ts
@@ -1,26 +1,27 @@
-export const dummyDatas = [
+import { Project } from "../../../interfaces/project"
+export const dummyDatas: Project[] = [
   {
     title: "front_js_practice",
     description: "Frontend development practice repository with JavaScript frameworks.",
-    langurage: "CSS",
+    language: "CSS",
     update_context: "Updated 2 days ago",
   },
   {
     title: "docker_practice",
     description: "",
-    langurage: "Go",
+    language: "Go",
     update_context: "Updated 2 days ago",
   },
   {
     title: "ruby_practice_basic",
     description: "ruby_practice_basic",
-    langurage: "Ruby",
+    language: "Ruby",
     update_context: "Updated 28 days ago",
   },
   {
     title: "practice_design",
     description: "design training repository (Site projection)",
-    langurage: "HTML",
+    language: "HTML",
     update_context: "Updated on 29 Aug",
   }
 ]
