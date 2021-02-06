f1ebfb10bd2840dde2a6d85346bc52cfd60411e3
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-02-06 21:56:08 +0900

    add components

---
+++ webpack-ts-react/src/sub-component.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+interface IProps {
+    name: string;
+}
+interface IState {
+    count: number;
+}
+
+export class SubComponent extends React.Component<IProps, IState> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0,
+        };
+    }
+
+    handleClick() {
+        console.log("クリックされました");
+        this.setState({
+            count: this.state.count + 1,
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>{this.props.name}</h2>
+                <div>{this.state.count}</div>
+                <button onClick={this.handleClick.bind(this)}>Add +1</button>
+            </div>
+        );
+    }
+}
