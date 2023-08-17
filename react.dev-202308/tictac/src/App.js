43174081957f52fad3b3c6f2256f2cfab80c4a6b
linnefromice <linnefromice@gmail.com>
2023-08-18 08:19:23 +0900

    clean

--- react.dev-202308/tictac/src/App.js
+++ react.dev-202308/tictac/src/App.js
@@ -74,24 +74,22 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = nextMove % 2 === 0
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
 
-  const moves = history.map((squares, move) => {
+  const moves = history.map((_squares, move) => {
     const description = move > 0
       ? "Go to move #" + move
       : "Go to game start";
