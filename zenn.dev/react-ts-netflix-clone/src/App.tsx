818c405aca1b8099b064d785ec52b55cb46290c5
linnefromice <linnefromice@gmail.com>
2023-08-18 17:18:32 +0900

    update: use axios & add Row component

--- zenn.dev/react-ts-netflix-clone/src/App.tsx
+++ zenn.dev/react-ts-netflix-clone/src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Row } from './components/Row';
+import { requests } from './utils/request';
 
 function App() {
   return (
     <div className="App">
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentMovies} />
     </div>
   );
 }
