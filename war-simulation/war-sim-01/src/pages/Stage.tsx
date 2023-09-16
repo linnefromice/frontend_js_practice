cd4f59fe8033904ee988a5d14e108925ae218c99
linnefromice <linnefromice@gmail.com>
2023-09-16 22:13:49 +0900

    update

--- war-simulation/war-sim-01/src/pages/Stage.tsx
+++ war-simulation/war-sim-01/src/pages/Stage.tsx
@@ -1,5 +1,21 @@
 import "./Stage.css"
 
+const ROW_NUM = 3
+const CELL_NUM_IN_ROW = 5
+
 export const Stage = () => {
-  return <div className="title">Stage</div>
+  return (
+    <div className="root">
+      <div className="stage">
+        {Array.from({ length: ROW_NUM }).map((_, i) => (
+          <div key={`row.${i}`} className="row">
+            {Array.from({ length: CELL_NUM_IN_ROW }).map((_, j) => {
+              const idx = i * ROW_NUM + j;
+              return <div key={`cell.${idx}`} className="cell">{idx}</div>
+            })}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
