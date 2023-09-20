9ce3bae9efb443c4ba2827226ff80d10b1b0606d
linnefromice <linnefromice@gmail.com>
2023-09-20 22:41:28 +0900

    update

--- war-simulation/war-sim-01/src/pages/Stage.tsx
+++ war-simulation/war-sim-01/src/pages/Stage.tsx
@@ -1,21 +1,75 @@
+import { useState } from "react";
 import "./Stage.css"
 
-const ROW_NUM = 3
-const CELL_NUM_IN_ROW = 5
+const INITIAL_COORDINATE = [3, 2]; // NOTE: coordinte start is 0
+const ROW_NUM = 5
+const CELL_NUM_IN_ROW = 7
+
+const ACTION_OPTIONS = ["MOVE", "ATTACK"] as const
+type ActionOptionType = typeof ACTION_OPTIONS[number]
 
 export const Stage = () => {
+  const [isOpenActionMenu, setIsOpenActionMenu] = useState(false)
+  const [activeActionOption, setActiveActionOption] = useState<ActionOptionType | null>(null)
+  const [coordinate, setCoordinate] = useState(INITIAL_COORDINATE)
+
   return (
     <div className="root">
       <div className="stage">
-        {Array.from({ length: ROW_NUM }).map((_, i) => (
-          <div key={`row.${i}`} className="row">
-            {Array.from({ length: CELL_NUM_IN_ROW }).map((_, j) => {
-              const idx = i * ROW_NUM + j;
-              return <div key={`cell.${idx}`} className="cell">{idx}</div>
+        {Array.from({ length: ROW_NUM }).map((_, y) => (
+          <div key={`row.${y}`} className="row">
+            {Array.from({ length: CELL_NUM_IN_ROW }).map((_, x) => {
+              const idx = x * ROW_NUM + y;
+              const isLocated = coordinate[0] === x && coordinate[1] === y;
+              const key = `cell.${idx}`
+              return isLocated ? (
+                <div
+                  key={key}
+                  className="cell cell-active"
+                  onClick={() => setIsOpenActionMenu(true)}
+                >
+                  <div className="cell-content">🤖</div>
+                </div>
+              )  : (
+                <div
+                  key={key}
+                  className="cell"
+                  onClick={activeActionOption == "MOVE"
+                    ? () => {
+                      setCoordinate([x, y])
+                      setActiveActionOption(null)
+                    }
+                    : undefined
+                  }
+                />
+              )
             })}
           </div>
         ))}
       </div>
+      {isOpenActionMenu && (
+        <div className="action-menu">
+          <ul>
+            <li>
+              <button onClick={() => setActiveActionOption("MOVE")}>移動</button>
+            </li>
+            <li>
+              {/* TODO: interact */}
+              <button onClick={() => setActiveActionOption("ATTACK")}>攻撃</button>
+            </li>
+            <li>
+              <button
+                onClick={() => {
+                  setActiveActionOption(null)
+                  setIsOpenActionMenu(false)
+                }}
+              >
+                閉じる
+              </button>
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
