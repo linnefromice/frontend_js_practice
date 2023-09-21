5a507264bff33943c041f747bc2811608234b693
linnefromice <linnefromice@gmail.com>
2023-09-21 21:44:33 +0900

    update

--- reffect.co.jp/reducer-understanging/src/App.tsx
+++ reffect.co.jp/reducer-understanging/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const reducer = (state, action) => {
+  if (action === 'INCREMENT') {
+    return { count: state.count + 1 };
+  } else {
+    return { count: state.count - 1 };
+  }
+}
+
+const initialState = { count: 0 };
+const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Counter</h1>
+      <h2>カウント: {state.count}</h2>
+      <button onClick={() => dispatch('INCREMENT')}>+</button>
+      <button onClick={() => dispatch('DECREMENT')}>-</button>
+    </div>
   )
 }
+
+const App = () => <Counter />
 
 export default App
