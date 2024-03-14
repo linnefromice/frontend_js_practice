d03c7cae2d87fcf07f4a3505b1f542712e9ebde1
linnefromice <linnefromice@gmail.com>
2024-03-14 21:17:31 +0900

    tutorial: url-search-param & synchronizing urls to form state & submitting forms onchange

--- remix.run/tutorial/app/root.tsx
+++ remix.run/tutorial/app/root.tsx
@@ -8,26 +8,38 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
-  useNavigation
+  useNavigation,
+  useSubmit
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node"
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node"
 import appStylesHref from "./app.css?url"
 
 import { getContacts, createEmptyContact } from "./data";
+import { useEffect } from "react";
 
 export const action = async () => {
   const contact = await createEmptyContact();
   return redirect(`/contacts/${contact.id}/edit`);
 }
 
-export const loader = async () => {
-  const contacts = await getContacts();
-  return json({ contacts });
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return json({ contacts, q });
 }
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
+  const { contacts, q } = useLoaderData<typeof loader>();
   const navigation = useNavigation();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value =  q || "";
+    }
+  }, [q])
 
   return (
     <html lang="en">
@@ -41,13 +53,18 @@ export default function App() {
         <div id="sidebar">
           <h1>Remix Contacts</h1>
           <div>
-            <Form id="search-form" role="search">
+            <Form
+              id="search-form"
+              role="search"
+              onChange={(event) => submit(event.currentTarget)}
+            >
               <input
-                id="q"
                 aria-label="Search contacts"
+                defaultValue={q || ""}
+                id="q"
+                name="q"
                 placeholder="Search"
                 type="search"
-                name="q"
               />
               <div id="search-spinner" aria-hidden hidden={true} />
             </Form>
