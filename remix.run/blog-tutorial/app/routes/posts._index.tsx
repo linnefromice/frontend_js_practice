c4680719a51a88b64b0cc4c8a0b4d594e5c603a2
linnefromice <linnefromice@gmail.com>
2024-03-18 18:13:25 +0900

    blog-tutorial: loading data

--- remix.run/blog-tutorial/app/routes/posts._index.tsx
+++ remix.run/blog-tutorial/app/routes/posts._index.tsx
@@ -1,7 +1,32 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
+import { getPosts } from "~/models/post.server";
+
+export const loader = async () => {
+  return json({
+    posts: await getPosts(),
+  })
+}
+
 const Posts = () => {
+  const { posts } = useLoaderData<typeof loader>();
+
   return (
     <div>
       <h1>Posts</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.slug}>
+            <Link
+              to={`/posts/${post.slug}`}
+              className="text-blue-600 underline"
+            >
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
