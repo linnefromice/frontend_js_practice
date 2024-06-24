66ab198b3d4a0c4760e9ce448be07edb141b4cf8
linnefromice <linnefromice@gmail.com>
2024-06-24 18:04:40 +0900

    update

--- tailwind_02/src/App.jsx
+++ tailwind_02/src/App.jsx
@@ -6,6 +6,12 @@ function App() {
     <div>
       <h1 className="text-3xl font-bold underline">Hello World!</h1>
       <ChatIcon />
+      <button className="text-base font-medium rounded-lg p-3 bg-slate-100 text-slate-900">
+        Decline
+      </button>
+      <button className="text-base font-medium rounded-lg p-3 bg-sky-500 text-white">
+        Accept
+      </button>
     </div>
   );
 }
