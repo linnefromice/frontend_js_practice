bbd01c966ed2612dae58f8a7869cdb3fc4fd05b2
linnefromice <linnefromice@gmail.com>
2024-06-24 17:56:08 +0900

    pure css pattern

--- tailwind_02/src/App.jsx
+++ tailwind_02/src/App.jsx
@@ -1,7 +1,31 @@
 import "./App.css";
+import "./chat-notification.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <ChatIcon />
+    </div>
+  );
 }
+
+const ChatIcon = () => {
+  return (
+    <div className="chat-notification">
+      <div className="chat-notification-logo-wrapper">
+        <img
+          className="chat-notification-logo"
+          src="/logos/chat-icon.png"
+          alt="Chat icon"
+        />
+      </div>
+      <div className="chat-notification-content">
+        <h4 className="chat-notification-title">ChitChat</h4>
+        <p className="chat-notification-message">You have a new message!</p>
+      </div>
+    </div>
+  );
+};
 
 export default App;
