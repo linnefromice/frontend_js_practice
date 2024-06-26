835239d4deff0c18709a550d20a340b349e10dff
linnefromice <linnefromice@gmail.com>
2024-06-26 18:14:27 +0900

    use radix-ui with styling

--- radix-ui-01/src/App.tsx
+++ radix-ui-01/src/App.tsx
@@ -1,11 +1,18 @@
+import * as Popover from '@radix-ui/react-popover'
 import './App.css'
+import "./Popover.css"
 
-function App() {
-
+const App = () => {
   return (
-    <>
-
-    </>
+    <Popover.Root>
+      <Popover.Trigger className="PopoverTrigger">More info</Popover.Trigger>
+      <Popover.Portal>
+        <Popover.Content className="PopoverContent">
+          Some more info…
+          <Popover.Arrow  className="PopoverArrow"/>
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   )
 }
 
