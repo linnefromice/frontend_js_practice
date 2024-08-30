442492425e8ba469b10c82acec7658996bd8bacf
linnefromice <linnefromice@gmail.com>
2024-08-30 14:19:37 +0900

    update

--- stripe/node-stripe/src/immediatePayment.ts
+++ stripe/node-stripe/src/immediatePayment.ts
@@ -6,6 +6,7 @@ if (!STRIPE_API_PUBLIC_KEY || !STRIPE_API_SECRET_KEY) {
   throw new Error("Missing Stripe API keys");
 }
 const stripe = new Stripe(STRIPE_API_SECRET_KEY);
+const CUSTOMER_ID = process.env.CUSTOMER_ID as string;
 
 const createProduct = async () => {
   const product = await stripe.products.create({
@@ -14,14 +15,6 @@ const createProduct = async () => {
   console.log(`Created product: ${product.id}`);
   return product;
 };
-
-console.log("Starting the script");
-createProduct()
-  .then(res => {
-    console.dir(res);
-    console.log("Finished the script");
-  })
-  .catch(console.error);
 
 // Created product: prod_QksYCv3uJnMuXu
 // {
@@ -46,3 +39,49 @@ createProduct()
 //   updated: 1724993424,
 //   url: null
 // }
+
+const previewInvoice = async () => {
+  const invoice = await stripe.invoices.createPreview({
+    invoice_items: [
+      {
+        price_data: {
+          product: "prod_QksYCv3uJnMuXu",
+          currency: "usd",
+          unit_amount: 200000, // note: set to calculate the total amount
+        },
+        quantity: 1,
+      },
+    ],
+  });
+  console.log(`Created invoice: ${invoice.id}`);
+  console.dir(invoice.lines.data[0]);
+  return invoice;
+};
+
+const createImmediateInvoice = async () => {
+  const invoice = await stripe.invoices.create({
+    customer: CUSTOMER_ID,
+    auto_advance: true,
+  });
+  await stripe.invoiceItems.create({
+    customer: CUSTOMER_ID,
+    price_data: {
+      product: "prod_QksYCv3uJnMuXu",
+      currency: "usd",
+      unit_amount: 200000,
+    },
+    invoice: invoice.id,
+  });
+  await stripe.invoices.pay(invoice.id);
+
+  const result = await stripe.invoices.retrieve(invoice.id);
+  console.log(`Created invoice: ${result.id}`);
+  console.dir(result.lines.data[0]);
+  return result;
+};
+
+console.log("Starting the script");
+createImmediateInvoice().then(res => {
+  console.dir(res);
+  console.log("Finished the script");
+});
